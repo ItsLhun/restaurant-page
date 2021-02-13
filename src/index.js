8da0b6836633895b0bf58859a8837c84e2295099
ItsLhun <nahuelghastin@gmail.com>
2021-02-14 00:13:29 +0100

    started working on nav bar and intro, no ext. libraries being used

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-document.addEventListener("click", (e) => {
-  alert("this has worked");
-});
+import { createIntro as createIntro } from "./intropage.js";
+import { menu } from "./menu.js";
+import { contact } from "./contact.js";
+
+const divContent = document.querySelector("#content");
+createIntro(divContent);
