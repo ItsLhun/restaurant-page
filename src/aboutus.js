147d13ea17a7c40aa4b12604cd34685e44949e5a
ItsLhun <nahuelghastin@gmail.com>
2021-02-17 00:12:40 +0100

    fixed about us layout, working on contact form

--- src/aboutus.js
+++ src/aboutus.js
@@ -7,7 +7,12 @@ const createAbout = (mainContent) => {
 
   const paraOne = document.createElement("p");
   paraOne.classList.add("aboutus-par-one");
-  paraOne.textContent = "lorem lorem lorem lorem ";
+  paraOne.textContent = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex praesentium perspiciatis eveniet similique maiores sequi laborum provident labore, necessitatibus impedit corporis magni cum illum quaerat reprehenderit doloremque, nemo vel natus.
+Unde, deserunt aperiam sunt aliquid qui ducimus, culpa assumenda debitis adipisci voluptatum, voluptates enim numquam porro sit ipsum. Doloremque, sit excepturi quibusdam asperiores accusantium magni quam expedita voluptatibus cupiditate porro?
+Assumenda magni repellat aperiam illum nemo id temporibus eius exercitationem, repellendus consequuntur facere! Alias blanditiis repellat at molestiae perspiciatis, enim hic veritatis soluta accusantium amet dolorem, quis dolor, assumenda illum.
+Ipsam minus facere, veniam totam neque accusantium cupiditate, expedita officia reprehenderit quidem, ab asperiores sapiente voluptatibus cum maxime aspernatur animi consequatur laboriosam esse? Veniam cupiditate tempore non beatae magnam nisi.
+Quas suscipit corporis fuga ipsam eius quis alias culpa! Perspiciatis neque modi reprehenderit repellendus labore expedita iure eos possimus temporibus nulla. Dolorum in possimus eos ad quam, atque eveniet natus?
+Nobis doloremque porro voluptatibus molestiae voluptate nihil itaque totam quos facere quibusdam debitis blanditiis officiis earum eaque sunt dicta reiciendis, temporibus ipsum, iusto culpa, quas deserunt doloribus. Rerum, maiores qui.`;
 
   aboutSection.appendChild(imgSide);
   aboutSection.appendChild(paraOne);
--- src/menu.js
+++ src/menu.js
@@ -2,24 +2,43 @@ const createMenu = (mainContent) => {
   const menuSection = document.createElement("section");
   menuSection.classList.add("page-section");
 
+  // TO-DO: create factory function for plates and sections
+
   const menuOne = document.createElement("div");
-  menuOne.textContent = "menu one";
+  const menuOneTitle = document.createElement("h2");
+  menuOneTitle.textContent = "Entrees";
+  menuOne.appendChild(menuOneTitle);
   menuOne.style.background = "red";
   menuOne.classList.add("menu-side");
 
   const menuTwo = document.createElement("div");
-  menuTwo.textContent = "menu two";
+  const menuTwoTitle = document.createElement("h2");
+  menuTwoTitle.textContent = "Main courses";
+  menuTwo.appendChild(menuTwoTitle);
+
   menuTwo.style.background = "green";
   menuTwo.classList.add("menu-side");
 
   const menuThree = document.createElement("div");
-  menuThree.textContent = "menu other";
+  const menuThreeTitle = document.createElement("h2");
+  menuThreeTitle.textContent = "Dessers";
+  menuThree.appendChild(menuThreeTitle);
+
   menuThree.style.background = "purple";
   menuThree.classList.add("menu-side");
+
+  const menuFour = document.createElement("div");
+  const menuFourTitle = document.createElement("h2");
+  menuFourTitle.textContent = "Drinks";
+  menuFour.appendChild(menuFourTitle);
+
+  menuFour.style.background = "purple";
+  menuFour.classList.add("menu-side");
 
   menuSection.appendChild(menuOne);
   menuSection.appendChild(menuTwo);
   menuSection.appendChild(menuThree);
+  menuSection.appendChild(menuFour);
 
   mainContent.appendChild(menuSection);
 };
